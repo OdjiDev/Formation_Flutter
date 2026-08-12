0406b8f5393cf17c7ee4582107f2b551ff1b4aa9
Djire <odjidev@gmail.com>
2026-08-12 18:07:25 +0000

    modification de  StatelessWidget qui n'actualise pas l'etat du compteur en StatefulWidget qui actualie  et le state qui relance le build

--- mon_premier_projet/lib/ui/pages/counter.page.dart
+++ mon_premier_projet/lib/ui/pages/counter.page.dart
@@ -3,15 +3,21 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/drawer.widget.dart';
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatefulWidget {
 
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
 int counter=0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer:MyDrawer(),
       appBar: AppBar(
-        title: Text('home'),
+        title: Text('Counter'),
       ),
       body: Center(
         child: Text('counter value  => ${counter}',
@@ -19,10 +25,12 @@ int counter=0;
               color: Colors.red,fontSize: 24),),
 
       ),
-    
+
     floatingActionButton: FloatingActionButton(
       onPressed: (){
-        counter++;
+       setState(() {
+         counter++;
+       });
         print('counter value  => $counter');
       },
       child: Icon(Icons.add),
